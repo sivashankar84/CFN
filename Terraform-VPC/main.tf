57bd5ffc8cdaf199438521d7f8112f9748c10e70
sivashankar84 <66830181+sivashankar84@users.noreply.github.com>
2024-04-15 19:38:27 +0100

    Update main.tf

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -3,14 +3,23 @@ data "aws_vpcs" "existing_vpcs" {
     Name = "Dev-vpc"
   }
 }
+
+data "aws_availability_zones" "available" {}
+
+
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = data.aws_vpcs.existing_vpcs.ids[0]
-  count                   = "${length(var.public_subnets_cidr)}"
+  count                   = length(data.aws_availability_zones.available.names) >= 2 ? 2 : length(data.aws_availability_zones.available.names)
   cidr_block              = "${element(var.public_subnets_cidr,   count.index)}"
-  availability_zone       = "${element(var.availability_zones,   count.index)}"
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
   tags = {
-    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-      public-subnet"
+    Name        = "${var.environment}-${element(data.aws_availability_zones.available.names, count.index)}-public-subnet"
     Environment = "${var.environment}"
   }
 }
+
+
+
+
+
