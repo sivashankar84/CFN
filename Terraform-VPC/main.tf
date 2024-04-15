c224f50fa5fdd1c6bac55ce6c58ce76e4fe959dc
sivashankar84 <66830181+sivashankar84@users.noreply.github.com>
2024-04-15 19:32:57 +0100

    Update main.tf

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -3,10 +3,14 @@ data "aws_vpcs" "existing_vpcs" {
     Name = "Dev-vpc"
   }
 }
-resource "aws_internet_gateway" "igw" {
-  vpc_id = data.aws_vpcs.existing_vpcs.ids[0]
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = data.aws_vpcs.existing_vpcs.ids[0]
+  count                   = "${length(var.public_subnets_cidr)}"
+  cidr_block              = "${element(var.public_subnets_cidr,   count.index)}"
+  availability_zone       = "${element(var.availability_zones,   count.index)}"
+  map_public_ip_on_launch = true
   tags = {
-    Name        = "${var.environment}-igw"
+    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-      public-subnet"
     Environment = "${var.environment}"
   }
 }
